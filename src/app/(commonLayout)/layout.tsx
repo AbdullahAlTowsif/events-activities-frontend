d0a0eb0119086eccde61d8f0b2a0615a568e4f89
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-12-05 19:34:34 +0600

    navbar added

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/shared/Navbar";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            {/* <PublicFooter /> */}
+        </>
+    );
+};
+
+export default CommonLayout;
