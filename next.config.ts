b595b9ebe4595ff586c16fbc21efeb10c635e65c
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-05-09 17:26:37 +0600

    body size limit increased

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,12 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
       }
     ]
-  }
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
